512bb1d745dd43fd5fee95a942673b21d118b0b7
Anna Kudriaeva <anna.kudriaeva@gmail.com>
2024-11-29 22:53:58 +0300

    minor fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,27 +1,22 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
 from dotenv import load_dotenv
-from page_analyzer.db import (
-    get_all_urls,
-    get_url_by_id,
-    get_checks_by_url_id,
-    insert_url,
-    get_url_by_name,
-    insert_check,
-)
+from page_analyzer import db
 from page_analyzer.utils import fetch_seo_data, normalize_url
 import os
-import psycopg2 as pg
-from psycopg2.extras import DictCursor
 import validators
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
 
 
-def get_db_connection():
-    return pg.connect(os.getenv("DATABASE_URL"), cursor_factory=DictCursor)
+@app.errorhandler(Exception)
+def handle_exception(e):
+    """Обработчик для глобальных ошибок."""
+    flash(f"Произошла ошибка: {e}", "error")
+    return redirect(url_for("index")), 500
 
 
 @app.route("/")
@@ -31,12 +26,12 @@ def index():
 
 @app.route("/urls", methods=["GET"])
 def get_urls():
+    conn = db.get_connection(app.config["DATABASE_URL"])
     try:
-        conn = get_db_connection()
-        urls = get_all_urls(conn)
-        return render_template("urls.html", urls=urls)
+        urls = db.get_all_urls(conn)
     finally:
         conn.close()
+    return render_template("urls.html", urls=urls)
 
 
 @app.post("/urls")
@@ -48,68 +43,68 @@ def post_url():
         flash("Некорректный URL", "error")
         return render_template("index.html"), 422
 
+    conn = db.get_connection(app.config["DATABASE_URL"])
     try:
-        conn = get_db_connection()
-        existing_url = get_url_by_name(conn, normalized_url)
-
+        existing_url = db.get_url_by_name(conn, normalized_url)
         if existing_url:
             flash("Страница уже существует", "error")
             return redirect(url_for("get_url", id=existing_url["id"]))
 
-        url_id = insert_url(conn, normalized_url)
+        url_id = db.insert_url(conn, normalized_url)
         conn.commit()
-
-        flash("Страница успешно добавлена", "success")
-        return redirect(url_for("get_url", id=url_id))
-
     except Exception as e:
-        flash(f"Ошибка добавления страницы: {e}", "error")
         conn.rollback()
+        flash(f"Ошибка добавления страницы: {e}", "error")
         return render_template("index.html"), 422
     finally:
         conn.close()
 
+    flash("Страница успешно добавлена", "success")
+    return redirect(url_for("get_url", id=url_id))
+
 
 @app.get("/urls/<int:id>")
 def get_url(id):
+    conn = db.get_connection(app.config["DATABASE_URL"])
     try:
-        conn = get_db_connection()
-        url = get_url_by_id(conn, id)
+        url = db.get_url_by_id(conn, id)
         if not url:
             flash("Ошибка при получении URL", "error")
             return redirect(url_for("index"))
 
-        checks = get_checks_by_url_id(conn, id)
-        numbered_checks = [(i + 1, check) for i, check in enumerate(checks)]
-        return render_template("url.html", url=url, checks=numbered_checks)
+        checks = db.get_checks_by_url_id(conn, id)
     finally:
         conn.close()
+
+    numbered_checks = [(i + 1, check) for i, check in enumerate(checks)]
+    return render_template("url.html", url=url, checks=numbered_checks)
 
 
 @app.post("/urls/<int:id>/check")
 def post_check_url(id):
+    conn = db.get_connection(app.config["DATABASE_URL"])
     try:
-        conn = get_db_connection()
-        url = get_url_by_id(conn, id)
+        url = db.get_url_by_id(conn, id)
         if not url:
             flash("Ошибка при проверке URL", "error")
             return redirect(url_for("index"))
 
         status_code, h1_content, title_content, meta_desc = fetch_seo_data(url["name"])
         if status_code is None:
+            flash("Не удалось проверить страницу", "error")
             return redirect(url_for("get_url", id=id))
 
-        insert_check(conn, id, status_code, h1_content, title_content, meta_desc)
+        db.insert_check(conn, id, status_code, h1_content, title_content, meta_desc)
         conn.commit()
-        flash("Страница успешно проверена", "success")
-        return redirect(url_for("get_url", id=id))
-
     except Exception as e:
-        flash(f"Ошибка проверки страницы: {e}", "error")
         conn.rollback()
+        flash(f"Ошибка проверки страницы: {e}", "error")
         return redirect(url_for("get_url", id=id))
     finally:
         conn.close()
+
+    flash("Страница успешно проверена", "success")
+    return redirect(url_for("get_url", id=id))
 
 
 if __name__ == "__main__":
